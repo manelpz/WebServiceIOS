cb7794b3c97453f338f72b52811ae5418c703a1a
emmanuel lopez <emmanuel.lopez.g@icloud.com>
2021-06-17 23:54:20 -0500

    Update ViewController.swift

--- WebServiceIOS/ViewController.swift
+++ WebServiceIOS/ViewController.swift
@@ -18,8 +18,6 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-       
     }
 
     @IBAction func SearchButton(_ sender: Any) {
